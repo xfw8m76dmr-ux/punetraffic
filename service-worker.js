a2babc3a1402c04751b13c7fa769013233eb6e18
xfw8m76dmr-ux <242397878+xfw8m76dmr-ux@users.noreply.github.com>
2026-01-12 12:54:13 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,14 @@
-self.addEventListener("install", () => {
+const SW_VERSION = "2026-01-12-3"; // CHANGE ON EVERY DEPLOY
+
+self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
-  return;
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
 });
